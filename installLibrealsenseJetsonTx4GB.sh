9fce815ec45dac2ca6badd2df3dd837ebed4b0d3
Sami Alex Hamici <samihamici@gmail.com>
2020-01-20 20:05:11 +0200

    Update installLibrealsenseJetsonTx4GB.sh

--- installLibrealsenseJetsonTx4GB.sh
+++ installLibrealsenseJetsonTx4GB.sh
@@ -1,2 +1,1 @@
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
-sudo update-grub && sudo reboot
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo reboot
