bb490c1ed94115ba5deaab6ce0baa8320074a980
samiha87 <samihamici@gmail.com>
2020-01-28 22:02:14 +0200

    Added more options

--- installUSBModem.sh
+++ installUSBModem.sh
@@ -1,10 +1,16 @@
+
 if [ $1 == "-m" ]; then
+	sudo apt install wvdial
 	if [ $2 == "HUAWEI" ]; then
-		sudo apt install wvdial
 		sudo cp wvdial/huawei_wvdial.conf /etc/wvdial.conf
 		echo "Settings installed for HUAWEI modem with elisa"
 		echo "run sudo wvdial to start"
+	if [ $2 == "NOKIA-CS15" ]; then
+		sudo cp wvdial/nokia_cs15_wvdial.conf /etc/wvdial.conf
+		echo "Settings installed for HUAWEI modem with elisa"
+                echo "run sudo wvdial to start"
 	else 
 		echo "Example ./installUSBModem.sh -m HUAWEI"
+		echo "Devices, HUAWEI, NOKIA-CS15"
 	fi
 fi
